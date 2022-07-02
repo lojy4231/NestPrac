0ea4e57ad590118179d06d7cc714f4e0456b95e0
lojy4231 <lojy4231@gmail.com>
2022-07-02 18:10:45 +0900

    모든게시물 불러오기

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+    // private를 쓰는 이유는 다른 컴포넌트에서 BoardsService에 접근해서 수정하는것을 방지하기 위해
+    private boards = [];
+
+    getAllBoards() {
+        return this.boards;
+    }
+}
