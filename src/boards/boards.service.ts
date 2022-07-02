c39f0658cc457133ad807e8492203cd8da736495
lojy4231 <lojy4231@gmail.com>
2022-07-02 18:36:09 +0900

    uuid 설치, 글쓰기 완성

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Board } from './board.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
@@ -7,5 +8,15 @@ export class BoardsService {
 
     getAllBoards(): Board[] {
         return this.boards;
+    }
+
+    createBoard(title: string, descripiton: string) {
+        const board:Board = {
+            id: uuid(),
+            title,
+            descripiton,
+        };
+        this.boards.push(board);
+        return board;
     }
 }
